b51503d73022eb42396e0f456aa20e15f6bcf4da
Vincent Wong <vwong@administrators-iMac-3.local>
2016-10-28 14:05:52 -0400

    modified upload.js

--- app/static/js/validation/custom_validators.js
+++ app/static/js/validation/custom_validators.js
@@ -3,7 +3,7 @@
  */
 
 window.Parsley.addValidator("maxFileSize", {
-  validateString: function(_value, maxSize, parsleyInstance) {
+  validateString(_value, maxSize, parsleyInstance) {
     var files = parsleyInstance.$element[0].files;
     return files.length !== 1  || files[0].size <= maxSize * 1000000;
   },
