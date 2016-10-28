cf95a592807b55ddf27c879f228b00149b018d14
Vincent Wong <vwong@administrators-iMac-3.local>
2016-10-28 12:33:08 -0400

    modified action-buttons-mobile-toggle.js

--- app/static/js/request/action-buttons-mobile-toggle.js
+++ app/static/js/request/action-buttons-mobile-toggle.js
@@ -1,9 +1,9 @@
 $(window).resize(function () {
     var width = $(window).width();
     if (width >= 768) {
-        $('#mobile-toggle').addClass('tabs-left');
+        $("#mobile-toggle").addClass("tabs-left");
     }
     else {
-        $('#mobile-toggle').removeClass('tabs-left');
+        $("#mobile-toggle").removeClass("tabs-left");
     }
 }).resize();
