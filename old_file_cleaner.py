1e98a43646c10833c99be4c3fb80fa8aea2a6cf0
sinsang <stefanick96@gmail.com>
2016-12-12 15:15:31 -0500

    fixes WIP:

--- old_file_cleaner.py
+++ old_file_cleaner.py
@@ -1,16 +1,19 @@
+import os
+import app.lib.file_utils as fu
+
+from datetime import datetime, timedelta
+
+from flask import current_app
 from app import create_app
 from app.constants import UPDATED_FILE_DIRNAME
 from app.models import Files
-from flask import current_app
-from datetime import datetime, timedelta
-import os
 
 
 def remove_files():
     upload_directory = current_app.config["UPLOAD_DIRECTORY"]
     searched_dir = UPDATED_FILE_DIRNAME
     curpath = os.path.abspath(os.walk(searched_dir))
-    for dirpath, dirnames, filenames in upload_directory:
+    for _, _, filenames in upload_directory:
         if Files in filenames:
             file_modified = datetime.timestamp(os.path.getmtime(searched_dir))
             if datetime.now() - file_modified > timedelta(hours=24):
